6046abd35ff87f3237990b708d2edc4f48d25e8c
Jacob Wren <46506182+JacobWren@users.noreply.github.com>
2020-12-08 16:32:13 -0800

    Add files via upload

---
+++ Projects/Project4/pmtests.py
@@ -0,0 +1,59 @@
+# Name:         Jacob Wren
+# Course:       CPE 101
+# Instructor:   Irene Humer
+# Assignment:   Pixel Magic
+# Term:         Winter 2020
+
+
+from Project5 import pixelmagic
+
+assert pixelmagic.main_small(['any', 'turn', 'original.ppm']) == None
+assert pixelmagic.main_small(['any', 'turn', 'original.ppm']) == None
+assert pixelmagic.main_small(['any', 'turn', 'original.ppm']) == None
+
+
+
+
+assert pixelmagic.main_small_2(['any', 'turn', 'original.ppm', '90']) == None
+assert pixelmagic.main_small_2(['any', 'turn', 'original.ppm', '90']) == None
+assert pixelmagic.main_small_2(['any', 'turn', 'original.ppm', '90']) == None
+
+
+
+
+assert pixelmagic.main_small_4(['any', 'turn', 'original.ppm', '90']) == None
+assert pixelmagic.main_small_4(['any', 'turn', 'original.ppm', '90']) == None
+assert pixelmagic.main_small_4(['any', 'turn', 'original.ppm', '90']) == None
+
+
+'''
+mode = 'test'
+
+
+#assert pixelmagic.read_pixels(file_name)
+
+
+assert pixelmagic.gray('name', mode, [1, 2, 3]) == []
+assert pixelmagic.gray('name', mode, [1, 4, 3]) == []
+assert pixelmagic.gray('name', mode, [1, 2, 5]) == []
+
+
+assert pixelmagic.fade('name', mode, [1, 2, 3], 1, 1, 1) == []
+assert pixelmagic.fade('name', mode, [1, 2, 4], 1, 1, 1) == []
+assert pixelmagic.fade('name', mode, [1, 5, 3], 1, 1, 1) == []
+
+
+assert pixelmagic.turn('name', mode, [6, 2, 3], 90) == []
+assert pixelmagic.turn('name', mode, [1, 2, 7], 90) == []
+assert pixelmagic.turn('name', mode, [1, 5, 3], 90) == []
+
+
+assert pixelmagic.flip('name', mode, [1, 9, 3]) == []
+assert pixelmagic.flip('name', mode, [1, 4, 3]) == []
+assert pixelmagic.flip('name', mode, [1, 2, 3]) == []
+
+
+assert pixelmagic.blur('name', mode, [1, 2, 3], 2) == []
+assert pixelmagic.blur('name', mode, [1, 2, 5], 2) == []
+assert pixelmagic.blur('name', mode, [5, 2, 3], 2) == []
+'''

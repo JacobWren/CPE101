1d1cf920aa021b49a336686f027d594a629d1573
Jacob Wren <46506182+JacobWren@users.noreply.github.com>
2020-12-08 18:15:15 -0800

    Update pbtests.py

--- ProblemSets/pbtests.py
+++ ProblemSets/pbtests.py
@@ -1,18 +1,14 @@
 # Name: Jacob Wren
 # Course: CPE 101
-# Instructor: Daniel Kauffman
-# Assignment: Problem set8
+# Instructor: Irene Humer
+# Assignment: Problem setb
 # Term: Winter 2020
 
-
 import psetb
-
 
 assert psetb.parse_term("Fall '20") == "Fall '20"
 assert psetb.parse_term("Spring '20") == "Spring '20"
 assert psetb.parse_term("Winter '20") == "Winter '20"
-
-
 
 assert psetb.parse_time("12:01:59PM") == '12:01:59PM'
 assert psetb.parse_time("12:01:59") == '12:01:59'

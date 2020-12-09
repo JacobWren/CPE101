17f6e1410f99e024bc716f30aa608e72bc3321bd
Jacob Wren <46506182+JacobWren@users.noreply.github.com>
2020-12-08 16:27:33 -0800

    Update calcudoku.py

--- Projects/Project3/calcudoku.py
+++ Projects/Project3/calcudoku.py
@@ -1,8 +1,9 @@
-# Name: Jacob Wren
+# Name:         Jacob Wren
 # Course:       CPE 101
 # Instructor:   Irene Humer
 # Assignment:   Calcudoku Project
 # Term:         Winter 2020
+
 
 def main():
     Num_cages = int(input())
@@ -15,29 +16,21 @@ def main():
     matx = make_zeros(n)
     short_matx = del_one(l)
     matx_1D = flatten(matx)
-    #print(matx_1D)
-    #current_matx = matx
-    
+ 
     i = 0
-    #print(matx_1D[i])
     while i < N:
-        #print("i:",i)
         index = i
         matx_1D[i] = matx_1D[i] + 1 # Increment the value in the current cell by 1 (starting from the top-left cell)
-        #print(matx_1D[i])
         if matx_1D[i] > n:
             matx_1D[i] = 0
             i = i - 1
-            #print("Subtract i")
         elif (valid_row(n, matx_1D, index) and valid_col(n, N, matx_1D, index)
               and invalid_too_big(l, index, matx_1D, short_matx) and
               invalid_not_equal(l, index, matx_1D, short_matx)):
             i = i + 1
-            #print("Increment i")
     for k in range(0, N, n):
         print( * matx_1D[k: k + n] , sep = ' ')
-        #print (', '.join(matx_1D))
-    #print(matx_1D)
+    
     
 # Max value in 1D list
 def find_max(ls):
@@ -53,6 +46,7 @@ def find_max(ls):
         count = count + 1
     return mx
 
+
 # Max value in 2D list
 def nest_list_max(l):
     count = 0
@@ -65,6 +59,7 @@ def nest_list_max(l):
             default = find_max(i)
         count = count + 1
     return int((int(default) + 1) ** (1 / 2)) 
+    
     
 # grid of zeros    
 def make_zeros(n):
@@ -83,7 +78,7 @@ def flatten(matx):
     return matx_1D
 
 
-def valid_row(n, matx_1D, index): #1
+def valid_row(n, matx_1D, index): 
     start = int((index // n) * n)
     # Check for duplicates
     mini = matx_1D[start: start + n]
@@ -92,7 +87,7 @@ def valid_row(n, matx_1D, index): #1
     return True
 
 
-def valid_col(n, N, matx_1D, index): #3
+def valid_col(n, N, matx_1D, index): 
     start = int(index % n)
     # Check for duplicates
     mini = matx_1D[start: start + N - n + 1: n]
@@ -112,25 +107,23 @@ def del_one(l):
 def cage_index(l, index, short_matx):
     j = 0
     for i in short_matx:
-        #print(index)
         if str(index) in i:
             return j
-            #print("found", j)
         j = j + 1
 
 
     
 def partial_full(l, index, short_matx):
     spot = cage_index(l, index, short_matx) # cage index
-    #print(spot)
     cage_length = len(short_matx[spot]) - 1 # cage length - 1
     inner_index = short_matx[spot].index(str(index)) # index of index in inner list
     if cage_length > inner_index:
         return True
     return False
 
+
 # only use if partial_full is True!
-def invalid_too_big(l, index, matx_1D, short_matx): #3
+def invalid_too_big(l, index, matx_1D, short_matx): 
     if partial_full(l, index, short_matx):
         spot = cage_index(l, index, short_matx) # cage index
         tot = int(l[spot][0]) # Sum of cage
@@ -147,8 +140,8 @@ def invalid_too_big(l, index, matx_1D, short_matx): #3
             return False
         return True
     return True
-#invalid_too_big([['5', '0', '5'], ['8', '1', '2', '6'], ['8', '3', '8'], ['6', '4', '9', '14'], ['13', '7', '12', '13'], ['5', '10', '15'], ['14', '11', '16', '20', '21'], ['6', '17', '18', '22'], ['10', '19', '23', '24']],2 , [4, 1, 2, 5, 3, 1, 5, 4, 3, 2, 2, 3, 5, 4, 1, 3, 4, 1, 2, 5, 5, 2, 3, 1, 4])
-#invalid_too_big([['5', '0', '5'], ['4', '1', '2', '6'], ['8', '3', '8'], ['6', '4', '9', '14'], ['13', '7', '12', '13'], ['5', '10', '15'], ['14', '11', '16', '20', '21'], ['6', '17', '18', '22'], ['10', '19', '23', '24']],2 , [4, 1, 2, 5, 3, 1, 5, 4, 3, 2, 2, 3, 5, 4, 1, 3, 4, 1, 2, 5, 5, 2, 3, 1, 4], [['0', '5'], ['1', '2', '6'], ['3', '8'], ['4', '9', '14'], ['7', '12', '13'], ['10', '15'], ['11', '16', '20', '21'], ['17', '18', '22'], ['19', '23', '24']])
+
+
 # check if we are at last value in the cage
 def last_value(l, index, short_matx):
     short_matx = short_matx # no total element
@@ -177,10 +170,5 @@ def invalid_not_equal(l, index, matx_1D, short_matx): #4
     return True
     
         
-  
-
 if __name__ == "__main__":
     main()
-
-
-    

5fb08ec26fe41e764ea9dfac2565bb49e0c57d0a
Jacob Wren <46506182+JacobWren@users.noreply.github.com>
2020-12-08 18:35:57 -0800

    Update pseta.py

--- ProblemSets/pseta.py
+++ ProblemSets/pseta.py
@@ -1,8 +1,9 @@
 # Name: Jacob Wren
 # Course: CPE 101
-# Instructor: Daniel Kauffman
+# Instructor: Irene Humer
 # Assignment: Problem set8
 # Term: Winter 2020
+
 
 from typing import Any, List, Tuple
 
@@ -18,7 +19,6 @@ def try_open(primary: str, secondary: str) -> List[str]:
         except FileNotFoundError:
             return []
 
-  
 
 def count_zeros(ints: List[int]) -> int:
     counter = 0
@@ -32,14 +32,6 @@ def count_zeros(ints: List[int]) -> int:
             return counter
         i = i + 1
         
-'''            
-def empty_list(chars):
-    q = 0
-    try:
-        chars[q]
-    except IndexError:
-        return False
-'''
 
 def try_isalnum(chars):
     j, q = 0, 0
@@ -48,7 +40,6 @@ def try_isalnum(chars):
     while True:
         try:
             int(chars[q])
-            #break
             q = q + 1
         except IndexError:
             return True
@@ -62,54 +53,6 @@ def try_isalnum(chars):
                 except ValueError:
                     j = j + 1
             q, j = q + 1, 0
-    #return True
-
-
-'''
-def try_isalnum(chars):
-    j, q = 0, 0
-    l = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
-     26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37]
-   # try:
-       # chars[q]
-   # except IndexError:
-    #    return False
-    while True:
-        try:
-            int(chars[q])
-            break
-        except IndexError:
-            return True
-        except ValueError:
-            while True:
-                try:
-                    int(chars[q], l[j])
-                    break
-                except IndexError:
-                    return False
-                except ValueError:
-                    j = j + 1
-            q, j = q + 1, 0
-    return True
-'''
-
-'''      
-def get_numeric_type(number):
-    try:
-        num = number - (number // 1)
-        1 / num
-        return "float"
-    except ZeroDivisionError:
-        return "int"
-'''
-
-'''
-def get_numeric_type(number):
-    try:
-        return type(number) is int
-    except ZeroDivisionError:
-        return "int"
-'''
 
 
 def get_numeric_type(number):
@@ -121,10 +64,8 @@ def get_numeric_type(number):
         return "float"
 
 
-
 def get_sequence_type(sequence):
     try:
-        #sequence[0] = 1
         sequence + []
         return "list"
     except TypeError:
@@ -135,83 +76,9 @@ def get_sequence_type(sequence):
             return "tuple"
 
 
-
 def are_same_type(x, y):
-    #try:
-        #x in [None] and y in [None]
-        #return True
     try:
         x + y
         return True
     except TypeError:
         return x is y
-        #return False
-    
-# (NoneType, string, list, or tuple)
-
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
